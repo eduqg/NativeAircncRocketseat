8df381258965dab3d4af31e2f4374938b977b471
eduqg <eduardoqgomes@gmail.com>
2019-10-03 13:29:21 -0300

    Assets and Login elements

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,10 +1,77 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, KeyboardAvoidingView, Text, TextInput, Image, StyleSheet, TouchableOpacity } from 'react-native';
+
+import logo from '../assets/logo.png';
 
 export default function Login() {
   return (
-    <View>
-      <Text>Hello World</Text>
+    <View style={styles.container}>
+      <Image source={logo} />
+      <View style={styles.form} >
+        <Text style={styles.label}>SEU E-MAIL *</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Seu e-mail"
+          placeholderTextColor="#999"
+          keyboardType="email-address"
+          autoCapitalize="none"
+          autoCorrect={false}
+        />
+        <Text style={styles.label}>TECNOLOGIAS *</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Seu e-mail"
+          placeholderTextColor="#999"
+          autoCapitalize="words"
+          autoCorrect={false}
+        />
+        {/* Estilização não herda do pai */}
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>Encontrar Spots</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
+
+// justifyContent: 'center', alinha tudo ao centro na vertical
+// alignItems: 'center', alinha na horizontal
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  label: {
+    fontWeight: 'bold',
+    color: '#444',
+    marginBottom: 8,
+  },
+  form: {
+    alignSelf: 'stretch',
+    paddingHorizontal: 30,
+    marginTop: 30,
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: '#ddd',
+    paddingHorizontal: 20,
+    fontSize: 16,
+    color: '#444',
+    height: 44,
+    marginBottom: 20,
+    borderRadius: 2,
+  },
+  button: {
+    height: 42,
+    backgroundColor: '#f05a5b',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 2,
+  },
+  buttonText: {
+    color: '#fff',
+    fontWeight: 'bold',
+    fontSize: 16,
+  },
+});
