5dec422cb24a064f359dd178594411b08b802209
eduqg <eduardoqgomes@gmail.com>
2019-10-03 12:33:57 -0300

    Creating pages

--- App.js
+++ App.js
@@ -4,16 +4,21 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello Aircnc!</Text>
+      <Text>Hello Aircnc!</Text>
     </View>
   );
 }
 
+// Toda estilização do react já é display:flex e flexDirection: column
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    transform: [
+      { rotateZ: '50deg' }
+    ]
   },
 });
