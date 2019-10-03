84c78b6810cc436ced111eb5dfd20f1edee2b0e1
eduqg <eduardoqgomes@gmail.com>
2019-10-03 16:26:59 -0300

    Initial style to SpotList

--- src/components/SpotList.js
+++ src/components/SpotList.js
@@ -1,17 +1,39 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { View, KeyboardAvoidingView, AsyncStorage, Platform, Text, TextInput, Image, StyleSheet, TouchableOpacity } from 'react-native';
 import api from '../services/api';
 export default function SpotList({ tech }) {
+  const [spots, setSpots] = useState([]);
+
   useEffect(() => {
     async function loadSpots() {
       const response = await api.get('/spots', {
         params: { tech }
       })
-
-      console.log(response.data)
+      setSpots(response.data);
     }
     loadSpots();
   }, []);
 
-  return <Text>{tech}</Text>
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Empresas que usam <Text style={styles.bold}>{tech}</Text></Text>
+
+    </View>
+  )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 30,
+  },
+  title: {
+    fontSize: 20,
+    color: '#444',
+    paddingHorizontal: 20,
+    marginBottom: 15,
+  },
+  bold: {
+    fontWeight: 'bold',
+  },
+
+});
